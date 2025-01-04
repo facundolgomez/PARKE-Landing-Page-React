b83c6595871654a6cbfc61d15f2dbe83b81d932b
facundolgomez <facundolgomez87@gmail.com>
2025-01-04 18:19:42 -0300

    video added to dashboard

--- src/components/dashboard/Dashboard.jsx
+++ src/components/dashboard/Dashboard.jsx
@@ -1,31 +1,31 @@
+import Video from "../video/Video";
+
 const Dashboard = () => {
-    return (
-        <>
-            <h1 className="pt-28">Menu de inicio</h1>
-            <h1>Menu de inicio</h1>
-            <h1>Menu de inicio</h1>
-            <h1>Menu de inicio</h1>
-            <h1>Menu de inicio</h1>
-            <h1>Menu de inicio</h1>
-            <h1>Menu de inicio</h1>
-            <h1>Menu de inicio</h1>
-            <h1>Menu de inicio</h1>
-            <h1>Menu de inicio</h1>
-            <h1>Menu de inicio</h1>
-            <h1>Menu de inicio</h1>
-            <h1>Menu de inicio</h1>
-            <h1>Menu de inicio</h1>
-            <h1>Menu de inicio</h1>
-            <h1>Menu de inicio</h1>
-            <h1>Menu de inicio</h1>
-            <h1>Menu de inicio</h1>
-            <h1>Menu de inicio</h1>
-            <h1>Menu de inicio</h1>
-            <h1>Menu de inicio</h1>
+  return (
+    <>
+      <div className="dashboard-container relative w-full h-[500px]">
+        <h1 className="absolute top-[90%] left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-white text-5xl z-10 font-medium flex flex-col items-center space-y-4">
+          <span>SOLUCIONES PARA EMBOLSADO,</span>
+          <span>PALETIZADO, ENVOLTURA y </span>
+          <span>TRANSPORTE</span>
+        </h1>
 
-        </>
-        
-    )
-}
+        <div className="flex justify-center items-center absolute bottom-[-150px] w-full z-10 space-x-4">
+          <button className="bg-[#6cdcf3] w-[250px] h-[50px] text-white font-bold whitespace-nowrap">
+            NUESTROS PRODUCTOS
+          </button>
+          <button
+            onClick={() => window.open("https://wa.me/3413708391", "_blank")}
+            className="bg-[#25D366] w-[200px] h-[50px] text-white font-bold"
+          >
+            WHATSAPP
+          </button>
+        </div>
 
-export default Dashboard
+        <Video />
+      </div>
+    </>
+  );
+};
+
+export default Dashboard;

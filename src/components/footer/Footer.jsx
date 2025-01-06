1c61abcbc8da605d6f9028180b891a579b11b2c5
facundolgomez <facundolgomez87@gmail.com>
2025-01-05 21:13:10 -0300

    footer component

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,5 +1,20 @@
 const Footer = () => {
+  return (
+    <div className="w-full flex justify-center items-start justify-around h-40  bg-[#030C20] text-white mt-20 pt-12 pb-96 text-xl">
+      <div>
+        <button className="flex basis-52 bg-[#030C20]">
+          <img
+            src="../../../public/logos/logoFooter.png"
+            width={250}
+            alt="Logo"
+          />
+        </button>
+      </div>
 
-}
+      <div>MAS SOBRE NOSOTROS</div>
+      <div>CONTACTO</div>
+    </div>
+  );
+};
 
-export default Footer
+export default Footer;

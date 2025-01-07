28af61d734ce1d4140ad894ac9a0802793dd615a
facundolgomez <facundolgomez87@gmail.com>
2025-01-07 15:41:48 -0300

    advances in footer

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -14,7 +14,7 @@ const Footer = () => {
     }
   };
   return (
-    <div className="w-full flex flex-col sm:flex-row justify-center items-start sm:justify-around bg-[#030C20] text-white mt-20 py-6 sm:py-12 text-lg sm:text-xl">
+    <div className="w-full flex flex-col sm:flex-row justify-center items-start sm:justify-around bg-[#030C20] text-white pt-6 pb-6 sm:pt-12 sm:pb-12 text-base sm:text-lg sm:gap-x-8 relative">
       <div>
         <button onClick={handleLogoClick} className="flex bg-[#030C20]">
           <img
@@ -25,12 +25,22 @@ const Footer = () => {
         </button>
       </div>
 
-      <div className="mt-4 sm:mt-0">MAS SOBRE NOSOTROS</div>
+      <ul className="mt-4 sm:mt-0 flex flex-col gap-4 px-4 list-disc marker:text-white-400 ">
+        <li className="no-hover  list-none font-bold">MAS SOBRE NOSOTROS</li>
+        <li className="hover:text-[#6cdcf3] cursor-pointer">Quiénes somos</li>
+        <li className="hover:text-[#6cdcf3] cursor-pointer">Noticias</li>
+        <li className="hover:text-[#6cdcf3] cursor-pointer">
+          Galeria de imagenes
+        </li>
+        <li className="hover:text-[#6cdcf3] cursor-pointer">
+          Preguntas frecuentes
+        </li>
+      </ul>
 
-      <div className="mt-4 sm:mt-0 flex flex-col gap-4">
-        <div>CONTACTO</div>
-        <div>Ruta AO12 Km 47.6 Roldán | Santa Fe | Argentina</div>
-        <div>
+      <ul className="mt-4 sm:mt-0 flex flex-col gap-4 list-disc marker:text-white-400 ">
+        <li className="no-hover list-none font-bold">CONTACTO</li>
+        <li>Ruta AO12 Km 47.6 Roldán | Santa Fe | Argentina</li>
+        <li>
           <a
             className="no-underline text-[#6cdcf3]"
             href="https://wa.me/3413708391"
@@ -38,17 +48,20 @@ const Footer = () => {
           >
             +54 3416456481
           </a>
-        </div>
-        <div>
+        </li>
+        <li>
           <a
             className="no-underline text-[#6cdcf3]"
             href="mailto:info@parkesrl.com.ar?body=My custom mail body"
           >
             info@parkesrl.com.ar
           </a>
-        </div>
-        <div>Lunes a viernes 8:00 a 17:00 hs</div>
-      </div>
+        </li>
+        <li>Lunes a viernes 8:00 a 17:00 hs</li>
+      </ul>
+      <p className="mt-auto text-sm text-gray-400 mt-4 sm:mt-0">
+        © {new Date().getFullYear()} PARKE. Todos los derechos reservados.
+      </p>
     </div>
   );
 };

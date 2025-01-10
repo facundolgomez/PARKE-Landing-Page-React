e4841f6d18dfc5fa8b3e283990dcc4efa591daba
facundolgomez <facundolgomez87@gmail.com>
2025-01-10 18:00:29 -0300

    fixing upScreen component

--- src/components/upscreen/UpScreen.jsx
+++ src/components/upscreen/UpScreen.jsx
@@ -1,21 +1,19 @@
-
-
-const UpScreen = () => {
+const UpScreen = ({ pathImage, title, paragraph }) => {
   return (
     <div
       className="relative h-96 bg-cover bg-center"
       style={{
-        backgroundImage: 'url("/imagenes-de-fondo/imagen-contacto-2.jpg")',
+        backgroundImage: `url(${pathImage})`,
       }}
     >
       <section className="absolute bottom-0 left-0 mb-4 bg-transparent dark:bg-dark">
         <div className="mx-auto sm:container">
           <div className="flex flex-col text-white">
             <h1 className="text-2xl font-semibold bg-sky-600 text-white px-4 py-2">
-              Contacto
+              {title}
             </h1>
             <p className="text-sm font-medium text-body-color justify-center items-center text-center dark:text-dark-6 bg-black px-4 py-2">
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+              {paragraph}
             </p>
           </div>
         </div>

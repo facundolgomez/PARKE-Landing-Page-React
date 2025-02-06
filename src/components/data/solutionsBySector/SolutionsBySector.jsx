376ac9a5a2d89f26fdf0615ca98d66353b8c8c8a
facundolgomez <facundolgomez87@gmail.com>
2025-02-06 16:55:45 -0300

    subsector data

---
+++ src/components/data/solutionsBySector/SolutionsBySector.jsx
@@ -0,0 +1,5 @@
+export const solutionsBySectors = {
+  agricultura: ["FERTILIZANTES", "SEMILLAS Y GRANOS", "VERDURAS"],
+  alimentosbalanceados: ["GRANULADOS", "POLVOS", "PELLETS"],
+  mineríayquímica: ["POLVOS", "GRANULADOS", "CARBON", "PELLETS"],
+};

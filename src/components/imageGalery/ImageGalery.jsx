73def07d248600c7b4535f9a0f84dce135be7c3d
facundolgomez <facundolgomez87@gmail.com>
2025-02-09 21:39:10 -0300

    ImageGallery modified

--- src/components/imageGalery/ImageGalery.jsx
+++ src/components/imageGalery/ImageGalery.jsx
@@ -1,17 +1,34 @@
 import { useState } from "react";
 import PropTypes from "prop-types";
 
-const ImageGallery = ({ images }) => {
+const ImageGallery = ({ images = [] }) => {
   const [index, setIndex] = useState(0);
+  const imagesPerPage = 5;
+
+  const handleNext = () => {
+    if (index + imagesPerPage < images.length) {
+      setIndex(index + imagesPerPage);
+    } else {
+      setIndex(0); // si llega al final reinicia
+    }
+  };
 
   return (
     <div className="text-center">
       <div className="flex gap-2 justify-center">
         {images.slice(index, index + 5).map((img, idx) => (
-          <img key={idx} src={img} alt={`Producto ${idx + 1}`} className="w-20 h-20 object-cover" />
+          <img
+            key={idx}
+            src={img}
+            alt={`Producto ${idx + 1}`}
+            className="w-20 h-20 object-cover"
+          />
         ))}
       </div>
-      <button className="mt-4 p-2 bg-blue-500 text-white rounded" onClick={() => setIndex((index + 5) % images.length)}>
+      <button
+        className="mt-4 p-2 bg-blue-500 text-white rounded"
+        onClick={handleNext}
+      >
         Siguiente
       </button>
     </div>
@@ -19,8 +36,7 @@ const ImageGallery = ({ images }) => {
 };
 
 ImageGallery.propTypes = {
-    images: PropTypes.arrayOf(PropTypes.string).isRequired,
-  };
-  
+  images: PropTypes.arrayOf(PropTypes.string).isRequired,
+};
 
 export default ImageGallery;

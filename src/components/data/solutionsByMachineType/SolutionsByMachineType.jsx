8d6833c48be33a3b522a501acd60117e767f653c
Gero <gerojbro@gmail.com>
2025-02-19 17:03:54 -0300

    SolutionsByMachineType object done

--- src/components/data/solutionsByMachineType/SolutionsByMachineType.jsx
+++ src/components/data/solutionsByMachineType/SolutionsByMachineType.jsx
@@ -1,34 +1,288 @@
 export const solutionsByMachineType = {
   embolsadoras: {
-    embolsadorasDeBolsas: {
-      bocaAbierta: {
-        "4 bolsas x minuto": ["EP5B-TC", "EP5B-BA", "EP5B-SF"],
-        "6 bolsas x minuto": ["EPB"],
-        "10 o más bolsas x minuto": ["EPN-BA"],
+    bocaAbierta: {
+      titles: ["EP5BA", "EP5B-TC", "EP5B-SF", "EPB", "EPN", "EEI"],
+      EP5BA: {
+        title: "titulo de la maquina",
+        image: "/img",
+        description: "descripion de la maquina",
       },
-      valvuladas: ["LBV SF", "LBV I"],
-      pequeñasBolsas: [],
+      EP5BTC: {
+        title: "Título de la maquina",
+        image: "/img",
+        description: "descripion de la maquina",
+      },
+      EP5BSF: {
+        title: "Título de la maquina",
+        image: "/img",
+        description: "descripion de la maquina",
+      },
+      EPB: {
+        title: "Título de la maquina",
+        image: "/img",
+        description: "descripion de la maquina",
+      },
+      EPN: {
+        title: "Título de la maquina",
+        image: "/img",
+        description: "descripion de la maquina",
+      },
+      EEI: {
+        title: "Título de la maquina",
+        image: "/img",
+        description: "descripion de la maquina",
+      },
     },
-    embolsadorasDeBigBag: ["EPB-BB", "EPN-BB", "EP5B-BB"],
+    valvuladas: {
+      titles: ["LBV SF", "LBV I"],
+      LBVSF: {
+        title: "Título de la maquina",
+        image: "/img",
+        description: "descripion de la maquina",
+      },
+      LBVI: {
+        title: "Título de la maquina",
+        image: "/img",
+        description: "descripion de la maquina",
+      },
+    },
+    bigBag: {
+      titulos: ["EPB-BB", "EPN-BB", "EP5B-BB"],
+      EPBBB: {
+        title: "Título de la maquina",
+        image: "/img",
+        description: "descripion de la maquina",
+      },
+      EPNBB: {
+        title: "Título de la maquina",
+        image: "/img",
+        description: "descripion de la maquina",
+      },
+      EP5BBB: {
+        title: "Título de la maquina",
+        image: "/img",
+        description: "descripion de la maquina",
+      },
+    },
+    pequeñasBolsas: {
+      titles: ["M1", "M2"],
+      M1: {
+        title: "Título de la maquina",
+        image: "/img",
+        description: "descripion de la maquina",
+      },
+      M2: {
+        title: "Título de la maquina",
+        image: "/img",
+        description: "descripion de la maquina",
+      },
+    },
   },
-  envolvedoras: {
-    envolvedorasDePalets: {
-      EPSA: ["AUTOMATICA", "SEMIAUTOMATICA"],
-      EPA: [],
+  costuraYSellado: {
+    cosedoras: {
+      titles: ["Hasta 8 bolsas x minuto", "Hasta 16 bolsas x minuto"],
+    },
+    hasta8xminuto: {
+      title: "Título de la maquina",
+      image: "/img",
+      description: "descripion de la maquina",
+    },
+    hasta16xminuto: {
+      title: "Título de la maquina",
+      image: "/img",
+      description: "descripion de la maquina",
+    },
+    selladoras: {
+      titles: ["TERMICA DE IMPULSO", "TERMICA CONTINUA"],
+      termicaImpulso: {
+        title: "Título de la maquina",
+        image: "/img",
+        description: "descripion de la maquina",
+      },
+      termicaContinua: {
+        title: "Título de la maquina",
+        image: "/img",
+        description: "descripion de la maquina",
+      },
     },
   },
   transportadores: {
-    transportadoresDeBolsas: ["TC3-PF", "TC4-PE", "Con derivador de bolsas"],
-    transportadoresDePallets: [],
-    transportadoresDeBigBag: [],
+    titles: ["Bolsas y cajas", "Pellets y Big Bag", "Granel", "Accesorios"],
+    bolsasYCajas: {
+      TCPF: {
+        title: "Título de la maquina",
+        image: "/img",
+        description: "descripion de la maquina",
+      },
+      TCPE: {
+        title: "Título de la maquina",
+        image: "/img",
+        description: "descripion de la maquina",
+      },
+    },
+    pelletsYBigBag: {
+      TCP: {
+        title: "Título de la maquina",
+        image: "/img",
+        description: "descripion de la maquina",
+      },
+      TPR: {
+        title: "Título de la maquina",
+        image: "/img",
+        description: "descripion de la maquina",
+      },
+      TBB: {
+        title: "Título de la maquina",
+        image: "/img",
+        description: "descripion de la maquina",
+      },
+    },
+    granel: {
+      TCG: {
+        title: "Título de la maquina",
+        image: "/img",
+        description: "descripion de la maquina",
+      },
+    },
+    accesorios: {
+      aplanadorDeBolsas: {
+        title: "Título de la maquina",
+        image: "/img",
+        description: "descripion de la maquina",
+      },
+      derivadordeBolsasOCajas: {
+        title: "Título de la maquina",
+        image: "/img",
+        description: "descripion de la maquina",
+      },
+    },
   },
-  cosedoras: ["Siruba AA6", "GK35"],
-  selladoras: ["Térmica", "Térmica continua"],
-  balanzas: [],
-  balanzasDosificadoras: [],
-  pesajesContinuos: [],
-  cajonesDePaletizado: [],
-  tolvasYTorresDeSustentacion: [],
-  zarandas: [],
-  desterronadores: [],
+  paletizado: {
+    titles: [
+      "Cajón de paletizado",
+      "Envolvedora de Pallets orbital",
+      "Envolvedora de Pallets de plato",
+    ],
+    cajónDePaletizado: {
+      title: "Título de la maquina",
+      image: "/img",
+      description: "descripion de la maquina",
+    },
+    envolvedoraDePalletsOrbital: {
+      EPA: {
+        title: "Título de la maquina",
+        image: "/img",
+        description: "descripion de la maquina",
+      },
+    },
+    envolvedoraDePalletsDePlato: {
+      EPSA: {
+        title: "Título de la maquina",
+        image: "/img",
+        description: "descripion de la maquina",
+      },
+    },
+  },
+
+  pesaje: {
+    titles: ["Ejes", "Balanzas", "Dosificadoras", "Pesaje continuo"],
+    ejes: {
+      PBP_30_T: {
+        title: "Título de la maquina",
+        image: "/img",
+        description: "descripion de la maquina",
+      },
+      PBP_M_2PL_30T: {
+        title: "Título de la maquina",
+        image: "/img",
+        description: "descripion de la maquina",
+      },
+      PBP_M_4PC_60T: {
+        title: "Título de la maquina",
+        image: "/img",
+        description: "descripion de la maquina",
+      },
+    },
+    balanzas: {
+      PBP_40X40: {
+        title: "Título de la maquina",
+        image: "/img",
+        description: "descripion de la maquina",
+      },
+      PBP_60X60: {
+        title: "Título de la maquina",
+        image: "/img",
+        description: "descripion de la maquina",
+      },
+      PBP_100X100: {
+        title: "Título de la maquina",
+        image: "/img",
+        description: "descripion de la maquina",
+      },
+      PBP_120X120: {
+        title: "Título de la maquina",
+        image: "/img",
+        description: "descripion de la maquina",
+      },
+      PBP_150X150: {
+        title: "Título de la maquina",
+        image: "/img",
+        description: "descripion de la maquina",
+      },
+    },
+    pesajeContinuo: {
+      PCS_1_10: {
+        title: "Título de la maquina",
+        image: "/img",
+        description: "descripion de la maquina",
+      },
+      PCS_35_50: {
+        title: "Título de la maquina",
+        image: "/img",
+        description: "descripion de la maquina",
+      },
+      PCS_50_100: {
+        title: "Título de la maquina",
+        image: "/img",
+        description: "descripion de la maquina",
+      },
+    },
+  },
+  acondicionamiento: {
+    titles: ["Zarandas", "Desterronadores"],
+    zarandas: {
+      Z3_200: {
+        title: "Título de la maquina",
+        image: "/img",
+        description: "descripion de la maquina",
+      },
+      Z2_100: {
+        title: "Título de la maquina",
+        image: "/img",
+        description: "descripion de la maquina",
+      },
+    },
+    desterronadores: {
+      DST: {
+        title: "Título de la maquina",
+        image: "/img",
+        description: "descripion de la maquina",
+      },
+    },
+  },
+  reciclaje: {
+    titles: ["Prensas compactadoras"],
+    prensasCompactadoras: {
+      PH_3: {
+        title: "Título de la maquina",
+        image: "/img",
+        description: "descripion de la maquina",
+      },
+      PH_5: {
+        title: "Título de la maquina",
+        image: "/img",
+        description: "descripion de la maquina",
+      },
+    },
+  },
 };

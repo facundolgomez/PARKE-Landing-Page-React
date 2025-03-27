05ed53d7f8c34732786af3adfb4b7cf5d23ffa39
facundolgomez <facundolgomez87@gmail.com>
2025-03-26 22:19:23 -0300

    solutionsBySector

--- src/components/data/solutionsBySector/SolutionsBySector.jsx
+++ src/components/data/solutionsBySector/SolutionsBySector.jsx
@@ -1,11 +1,52 @@
-export const solutionsBySectors = {
-  reciclajeyorganicos: ["EMBOLSADO", "COMPACTADO", "EMBOLSADO Y COMPACTADO"],
-  agricultura: [
-    "FERTILIZANTES GRANULARES",
-    "FERTILIZANTES POLVOS",
-    "SEMILLAS GRANOS Y OLEAGINOSAS",
-  ],
-  alimentacionhumana: ["POLVOS", "IRREGULARES"],
-  alimentosbalanceados: ["GRANULADOS", "POLVOS"],
-  mineríayquímica: ["POLVOS", "GRANULADOS"], // Sin tilde
-};
+//ESTE ARCHIVO SE PUEDE ELIMINAR
+
+// // export const solutionsBySectors = {
+// //   reciclajeyorganicos: ["EMBOLSADO", "COMPACTADO", "EMBOLSADO Y COMPACTADO"],
+// //   agricultura: [
+// //     "FERTILIZANTES GRANULARES",
+// //     "FERTILIZANTES POLVOS",
+// //     "SEMILLAS GRANOS Y OLEAGINOSAS",
+// //   ],
+// //   alimentacionhumana: ["POLVOS", "IRREGULARES"],
+// //   alimentosbalanceados: ["GRANULADOS", "POLVOS"],
+// //   mineríayquímica: ["POLVOS", "GRANULADOS"], // Sin tilde
+// // };
+// export const solutionsBySectors = {
+//   reciclajeyorganicos: {
+//     es: ["EMBOLSADO", "COMPACTADO", "EMBOLSADO Y COMPACTADO"],
+//     en: ["BAGGED", "COMPACTED", "BAGGED AND COMPACTED"],
+//     pt: ["EMBALADO", "COMPACTADO", "EMBALADO E COMPACTADO"],
+//   },
+//   agricultura: {
+//     es: [
+//       "FERTILIZANTES GRANULARES",
+//       "FERTILIZANTES POLVOS",
+//       "SEMILLAS GRANOS Y OLEAGINOSAS",
+//     ],
+//     en: [
+//       "GRANULAR FERTILIZERS",
+//       "POWDER FERTILIZERS",
+//       "SEEDS GRAINS AND OILSEEDS",
+//     ],
+//     pt: [
+//       "FERTILIZANTES GRANULARES",
+//       "FERTILIZANTES EM PÓ",
+//       "SEMENTES GRÃOS E OLEAGINOSAS",
+//     ],
+//   },
+//   alimentacionhumana: {
+//     es: ["POLVOS", "IRREGULARES"],
+//     en: ["POWDERS", "IRREGULARS"],
+//     pt: ["PÓS", "IRREGULARES"],
+//   },
+//   alimentosbalanceados: {
+//     es: ["GRANULADOS", "POLVOS"],
+//     en: ["GRANULATED", "POWDERS"],
+//     pt: ["GRANULADOS", "PÓS"],
+//   },
+//   mineriayquimica: {
+//     es: ["POLVOS", "GRANULADOS"],
+//     en: ["POWDERS", "GRANULATED"],
+//     pt: ["PÓS", "GRANULADOS"],
+//   },
+// };
